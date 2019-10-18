1bff706c282d7b1fa2d68c1ad10072bf2a4a36e6
LZylstra <lindseyzylstra@gmail.com>
2019-10-17 22:32:26 -0700

    updates html, adds event listeners to buttons, adds calls to APIs

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
 const apiKeyMealDB = 1;
-const apiKeyYelp = "ad10642a117f6a43eda80076a5e92fa6";
-const searchURLMealDB = "https://www.themealdb.com/api/json/v1/1/filter.php"; //param c=Seafood
-const searchURLYelp = "https://api.yelp.com/v3/businesses/search";
+const apiKeyYelp = "MzC-vR8dGg4sB93woVcMeZoy_2-6iX1EQv9bCUev0uQJRuIbRuO-1K6R4JmaAiSv8yLQZtFofBKQrLG1zrq80dFTVwKJ3Zfs44fmJM2sgoSXNYkeXO0-xIUS8kapXXYx";
+const searchURLMealDB = "https://www.themealdb.com/api/json/v1/1/filter.php"; 
+const searchURLYelp = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search";
 const categories = ["Beef", "Chicken", "Dessert", "Lamb", "Miscellaneous", "Pasta", "Pork", "Seafood", "Side", "Starter", "Vegan", "Vegetarian", "Breakfast", "Goat"];
+let decision;
 
-
-function getMeal(btn){
-    $('.start').css("display", "none");
-    if (btn === "out"){
-        eatOut();
-    }
-    if (btn === "home"){
-        eatIn();
-    }
-}
+function formatQueryParams(params) {
+    const queryItems = Object.keys(params)
+      .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+    return queryItems.join('&');
+  }
 
 function eatOut(){
-    $('#right-box').append(`
-    <label for = "food-type" class = "eat-out">Type of Food: </label>
-    <input type = "text" name = "food-type" class = "eat-out">
+    decision = "resturant";
+    $('#left-box').append(`
+    <form class = "eat-form">
+        <label for = "food-type" class = "eat-out">Type of Food: </label>
+        <input type = "text" name = "food-type" class = "eat-out" id = "food-type-chosen">
 
-    <label for = "location" class = "eat-out">Location: </label>
-    <input type = "text" name = "location" class = "eat-out">
+        <label for = "location" class = "eat-out">Location: </label>
+        <input type = "text" name = "location" class = "eat-out" id = "location-chosen">
 
-    <label for = "open-now" class = "eat-out">Only show currently open resturants </label>
-    <input type = "checkbox" name = "open-now" class = "eat-out">
+        <label for = "open-now" class = "eat-out">Only show currently open resturants </label>
+        <input type = "checkbox" name = "open-now" class = "eat-out" id = "onlyopen">
 
-    <input type = "submit" value = "Random" id = "random" class = "eat-out">
-    <input type = "submit" value = "Search" id = "search" class = "eat-out">
+        <button class = "eat-out random">Random </button>
+        <button class = "eat-out search">Search </button>
+    </form>
     `);
 }
 
 function eatIn(){
+    decision = "cook";
     let categoryString;
 
     for (let i = 0; i < categories.length; i++){
@@ -41,29 +41,143 @@ function eatIn(){
     $('#categories').append(categoryString);
     
     $('#left-box').append(
-    `<input type = "submit" value = "Random" id = "random" class = "eat-in">
-    <input type = "submit" value = "Search" id = "search" class = "eat-in">
+    `<form class = "eat-form">
+        <button class = "eat-in random">Random </button>
+        <button class = "eat-in search">Search </button>
+    </form>
     `);
     $('#categories').removeClass('hidden');
 }
 
+function getResults(foodTypeChoice, locationChoice, opennow){
+    let url;
+    if (decision === "cook"){
+        url = searchURLMealDB + "?c=" + foodTypeChoice;
+        console.log(url);
+
+        fetch(url)
+        .then(response =>{
+            if (response.ok){
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => displayResults(responseJson))
+        .catch(err =>{
+            $('#js-error-message').text(`Something went wrong: ${err.message}`);
+        });
+    }
+    if (decision === "resturant"){
+        const params = {
+            term: foodTypeChoice,
+            location: locationChoice,
+            open_now: opennow
+        }
+        const queryString = $.param(params);
+        url = searchURLYelp + '?' + queryString;
+        console.log(url);
+        fetch(url, {headers: {
+            "accept": "application/json",
+            "x-requested-with": "XMLHttpRequest",
+            "Access-Control-Allow-Origin":"*",
+            "Authorization": `Bearer ${apiKeyYelp}`}})
+        .then(response =>{
+            if (response.ok){
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => displayResults(responseJson))
+        .catch(err =>{
+            $('#js-error-message').text(`Something went wrong: ${err.message}`);
+        });
+    }
+}
+
+function displayResults(responseJson){
+    console.log(responseJson)
+}
+/*
+function getResturants(foodTypeChoice, locationChoice, opennow){
+    fetch(url)
+        .then(response =>{
+            if (response.ok){
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => displayRecipes(responseJson))
+        .catch(err =>{
+            $('#js-error-message').text(`Something went wrong: ${err.message}`);
+        }   
+}
+
+function getRecipes(foodTypeChoice){
+    fetch(url)
+        .then(response =>{
+            if (response.ok){
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => displayRecipes(responseJson))
+        .catch(err =>{
+            $('#js-error-message').text(`Something went wrong: ${err.message}`);
+        }
+}*/
+
 function watchForm(){
-    $('form').submit(event => {
+    let locationChoice;
+    let foodTypeChoice;
+    let opennow;
+    $('#options').on("click", "#home-btn", function(event){
         event.preventDefault();
-        var btn = document.activeElement.id;
-        getMeal(btn);
-    })
+        $('.start').css("display", "none");
+        eatIn();
+    });
+
+    $('#options').on("click", "#out-btn", function(event){
+        event.preventDefault();
+        $('.start').css("display", "none");
+        eatOut();
+    });
+
+    $('#left-box').on( "click", ".random", function(event){
+        event.preventDefault();
+        console.log("random pressed");
+    });
+
+    $('#left-box').on( "click", ".search", function(event){
+        event.preventDefault();
+        console.log("search pressed");
+        if (decision === "cook"){
+            foodTypeChoice = $('#categories').val();
+           // console.log(foodTypeChoice);
+           getResults(foodTypeChoice);
+        }
+        if (decision === "resturant"){
+            foodTypeChoice = $('#food-type-chosen').val();
+           // console.log(foodTypeChoice);
+            locationChoice = $('#location-chosen').val();
+           // console.log(locationChoice);
+            opennow = $("#onlyopen")[0].checked;
+            //console.log(opennow);
+            getResults(foodTypeChoice, locationChoice, opennow);
+        }
+    });
+    watchHeader();
 }
 
 function watchHeader(){
     $('#banner').on('click', '.reset-to-home', function(event){
+        console.log("going in watch header");
         event.preventDefault();
         $('.start').css("display", "inline-block");
         $('#categories').addClass('hidden');
+        $('#categories').empty();
         $('.eat-out').css("display", "none");
         $('.eat-in').css("display", "none");
     })
 }
 
 $(watchForm);
-$(watchHeader)
